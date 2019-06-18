66049fae8d98d21d89e637b9238426a70146f450
Seunghoon Lee <kneeprayer@gmail.com>
2019-06-18 12:45:44 +0900

    bugfix #2.25 More Controlers

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,7 @@
 import { videos } from "../db";
 import routes from "../routes";
-
 export const home = (req, res) => {
-    res.render("home", { pageTitle: "Home" });
+    res.render("home", { pageTitle: "Home", videos });
 };
 
 export const search = (req, res) => {
@@ -16,11 +15,11 @@ export const getUpload = (req, res) =>
     res.render("upload", { pageTitle: "Upload" });
 
 export const postUpload = (req, res) => {
-  const {
-    body: { file, title, description }
-  } = req;
-  // To Do: Upload and save video
-  res.redirect(routes.videoDetail(324393));
+    const {
+        body: { file, title, description }
+    } = req;
+    // To Do: Upload and save video
+    res.redirect(routes.videoDetail(324393));
 };
 
 export const videoDetail = (req, res) =>
