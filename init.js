78f6743dcd7128989387d3e6031dcd1b316cbeb3
Seunghoon Lee <kneeprayer@gmail.com>
2019-06-19 12:30:46 +0900

    #3.4 Comment Model

--- init.js
+++ init.js
@@ -1,7 +1,9 @@
 import "./db";
 import app from "./app";
 import dotenv from "dotenv";
+import "./models/Video";
 
+dotenv.config();
 const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
