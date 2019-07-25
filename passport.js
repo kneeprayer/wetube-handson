448bdd6f57e740f62c5d296af8e9b33eb42e6dbe
Seunghoon Lee <kneeprayer@gmail.com>
2019-07-26 03:23:51 +0900

    #6.6 Github Log In part One

--- passport.js
+++ passport.js
@@ -3,5 +3,14 @@ import User from "./models/User";
 
 passport.use(User.createStrategy());
 
+passport.use(
+  new GithubStrategy({
+    clientID: process.env.GH_ID,
+    clientSecret: process.env.GH_SECRET,
+    callbackURL: "http://localhost:4000/auth/github/callback"
+  }),
+  githubLoginCallback
+);
+
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
