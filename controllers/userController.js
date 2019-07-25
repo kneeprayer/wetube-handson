350af150caf63f91d644bca3641fec5812156f3f
Seunghoon Lee <kneeprayer@gmail.com>
2019-07-26 04:01:55 +0900

    #7.0 User Profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -117,7 +117,7 @@ export const userDetail = async (req, res) => {
   }
 };
 
-export const editProfile = (req, res) =>
+export const getEditProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "Edit Profile" });
 
 export const changePassword = (req, res) =>
