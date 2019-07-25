1238e110bbb2a9e2e3375d004f5fc70dc5a6e4e0
Seunghoon Lee <kneeprayer@gmail.com>
2019-07-26 03:04:48 +0900

    #6.1 Local Authentication with Passport part One

--- init.js
+++ init.js
@@ -1,9 +1,13 @@
-import "./db";
 import dotenv from "dotenv";
+import "./db";
 import app from "./app";
+
 import "./models/Video";
+import "./models/Comment";
+import "./models/User";
 
 dotenv.config();
+
 const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
