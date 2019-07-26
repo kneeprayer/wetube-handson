3cd201efc8a2ca25b356e0492db9a92f495aa209
Seunghoon Lee <kneeprayer@gmail.com>
2019-07-26 13:09:20 +0900

    #11.4 Building for Production

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import bodyParser from "body-parser";
 import passport from "passport";
 import mongoose from "mongoose";
 import session from "express-session";
+import path from "path";
 import MongoStore from "connect-mongo";
 import { localsMiddleware } from "./middlewares";
 import routes from "./routes";
@@ -22,19 +23,19 @@ const CookieStore = MongoStore(session);
 
 app.use(helmet());
 app.set("view engine", "pug");
-app.use("/uploads", express.static("uploads"));
-app.use("/static", express.static("static"));
+app.set("views", path.join(__dirname, "views"));
+app.use("/static", express.static(path.join(__dirname, "static")));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(
-  session({
-    secret: process.env.COOKIE_SECRET,
-    resave: true,
-    saveUninitialized: false,
-    store: new CookieStore({ mongooseConnection: mongoose.connection })
-  })
+    session({
+        secret: process.env.COOKIE_SECRET,
+        resave: true,
+        saveUninitialized: false,
+        store: new CookieStore({ mongooseConnection: mongoose.connection })
+    })
 );
 
 app.use(passport.initialize());
--- src/init.js
+++ src/init.js
@@ -1,3 +1,4 @@
+import "@babel/polyfill";
 import dotenv from "dotenv";
 import "./db";
 import app from "./app";
@@ -11,6 +12,6 @@ dotenv.config();
 const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
-  console.log(`✅  Listening on: http://localhost:${PORT}`);
+    console.log(`✅  Listening on: http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
